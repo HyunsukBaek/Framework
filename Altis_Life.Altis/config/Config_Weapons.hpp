0ead7151daaeeebd5a28f4df5d7ca1c333450c68
SoulFuxxer <koreamixdj@naver.com>
2017-05-02 03:25:03 +0900

    조준경 달린 아이템 삭제

--- Altis_Life.Altis/config/Config_Weapons.hpp
+++ Altis_Life.Altis/config/Config_Weapons.hpp
@@ -159,7 +159,7 @@ class WeaponShops {
             { "hgun_P07_F", "", 7500, 3750, "" },
             { "hgun_P07_khk_F", "", 7500, 3750, "" }, //Apex DLC
             { "hgun_Pistol_heavy_01_F", "", 9500, 4750, "call life_coplevel >= 1" },
-            { "SMG_02_ACO_F", "", 30000, 15000, "call life_coplevel >= 2" },
+            { "SMG_02_F", "", 30000, 15000, "call life_coplevel >= 2" },
             { "arifle_MX_F", "", 35000, 17500, "call life_coplevel >= 2" },
             { "hgun_ACPC2_F", "", 17500, 8750, "call life_coplevel >= 3" },
             { "arifle_MXC_F", "", 30000, 15000, "call life_coplevel >= 3" },
